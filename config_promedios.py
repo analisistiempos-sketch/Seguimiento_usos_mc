3aca0acc6d497245384f91c2880f4448b64a998d
analisistiempos-sketch <analisistiempos@gmail.com>
2026-08-17 01:33:42 -0500

    ultima

--- config_promedios.py
+++ config_promedios.py
@@ -146,6 +146,7 @@ LINEAS = [
 
     {"mostrar": True,  "fecha": "2026-08-09", "color": COLORES["azul_cielo_claro"], "estilo": "solid", "nombre": "Domingo 09"},
     {"mostrar": True,  "fecha": "2026-08-14", "color": COLORES["azul_corporativo"], "estilo": "dash", "nombre": "Viernes 14"},
+    {"mostrar": True,  "fecha": "2026-08-15", "color": COLORES["verde_menta"], "estilo": "dash", "nombre": "Sábado 15"},
     {"mostrar": True,  "fecha": "2026-08-16", "color": COLORES["violeta"], "estilo": "dash", "nombre": "Domingo 16"},
     {"mostrar": True,  "fecha": "2026-08-17", "color": COLORES["morado_oscuro"], "estilo": "solid",  "nombre": "Lunes 17"},
 ]
