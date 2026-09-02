4684865a8338cb19be957d3b736e96c9a34563ee
Miguel Ortiz <analisistiempos@gmail.com>
2026-09-02 08:31:53 -0500

    02_sept

--- config_promedios.py
+++ config_promedios.py
@@ -105,8 +105,8 @@ LINEAS = [
     {'mostrar': True, 'desde': '2026-08-24', 'hasta': '2026-08-28', 'color': '#FF80AB', 'estilo': 'dash', 'nombre': 'Promedio 24-28 ago'}, 
     
     
-    {'mostrar': True, 'fecha': '2026-08-31', 'color': '#E377C2', 'estilo': 'dash', 'nombre': '31_agosto'},    
-    {'mostrar': True, 'fecha': '2026-09-01', 'color': '#4B0082', 'estilo': 'solid', 'nombre': '01_septiembre'},
+    {'mostrar': True, 'fecha': '2026-09-01', 'color': '#E377C2', 'estilo': 'dash', 'nombre': '01_septiembre'},    
+    {'mostrar': True, 'fecha': '2026-09-02', 'color': '#4B0082', 'estilo': 'solid', 'nombre': '02_septiembre'},
     ]
 '''
 # Sabado
